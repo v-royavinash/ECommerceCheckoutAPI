b1f1127ac14eb082bb70bf83f8897a07fa5fb63b
Avinash Roy <v-royavinash@microsoft.com>
2023-07-27 00:18:03 -0700

    Added the base skeleton of the ECommerce API Solution

---
+++ ECommerceCheckoutAPI/Controllers/CheckoutController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ECommerceCheckout.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ECommerceCheckoutAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly ICheckoutService _checkoutService;
+        private readonly ILogger<CheckoutController> _logger;
+
+        public CheckoutController(ICheckoutService checkoutService, ILogger<CheckoutController> logger)
+        {
+            _checkoutService = checkoutService ?? throw new ArgumentNullException(nameof(checkoutService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(decimal), 200)]
+        [ProducesResponseType(400)]
+        public ActionResult<decimal> Post([FromBody] List<string> watchIds)
+        {
+            // Implement the logic to calculate the total cost for a list of watches
+            return 360;
+        }
+    }
+}
---
+++ ECommerceCheckoutAPI/Startup.cs
@@ -0,0 +1,70 @@
+using ECommerceCheckout.Domain;
+using ECommerceCheckout.Domain.Models;
+using ECommerceCheckout.Domain.Services;
+using ECommerceCheckoutAPI.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+
+namespace ECommerceCheckoutAPI
+{
+    /// <summary>
+    /// Startup class to configure the ASP.NET Core Web API application.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration instance.</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the configuration instance.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// This method to configure services.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(AzureADDefaults.BearerAuthenticationScheme)
+                .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
+            services.AddControllers();
+            services.AddScoped<ICheckoutService, CheckoutService>();
+        }
+
+        /// <summary>
+        /// The configure method.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
