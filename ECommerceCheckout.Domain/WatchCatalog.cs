dcf3969ada8361693632307c89adbb02c3016c47
Avinash Roy <v-royavinash@microsoft.com>
2023-07-27 00:48:44 -0700

    Added a json reader to read the watch prices.

---
+++ ECommerceCheckout.Domain/WatchCatalog.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ECommerceCheckout.Domain.Models;
+using ECommerceCheckout.Utilities;
+
+namespace ECommerceCheckout.Domain
+{
+    /// <summary>
+    /// Represents a catalog of watches with their associated prices.
+    /// </summary>
+    public static class WatchCatalog
+    {
+        /// <summary>
+        /// Gets the list of watches in the catalog.
+        /// </summary>
+        public static readonly List<Watch> Watches;
+
+        static WatchCatalog()
+        {
+            string baseDirectory = Directory.GetCurrentDirectory();
+            string domainProjectDirectory = Path.GetFullPath(Path.Combine(baseDirectory, "..", "ECommerceCheckout.Domain"));
+            string jsonFilePath = Path.Combine(domainProjectDirectory, "Data", "watch_catalog.json");
+
+            Watches = JsonFileReader.ReadJsonFile<List<Watch>>(jsonFilePath);
+        }
+
+        /// <summary>
+        /// Gets a watch from the catalog by its ID.
+        /// </summary>
+        /// <param name="watchId">The ID of the watch to retrieve.</param>
+        /// <returns>The watch with the specified ID, or null if not found.</returns>
+        public static Watch GetWatchById(string watchId)
+        {
+            return Watches.FirstOrDefault(watch => watch.WatchId == watchId);
+        }
+    }
+}
---
+++ ECommerceCheckout.Utilities/Helpers/JsonFileReader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace ECommerceCheckout.Utilities
+{
+    /// <summary>
+    /// A utility class to read JSON data from a file.
+    /// </summary>
+    public static class JsonFileReader
+    {
+        /// <summary>
+        /// Reads JSON data from the specified file and deserializes it to the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize the JSON data into.</typeparam>
+        /// <param name="filePath">The path to the JSON file.</param>
+        /// <returns>The deserialized object of type T.</returns>
+        public static T ReadJsonFile<T>(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
